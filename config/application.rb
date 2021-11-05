d5db4ed16e4622416dc7740144ec20c00a6ddb10
teru <titlt.aiw6@gmail.com>
2021-11-05 13:03:34 +0900

    エラーの修正

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module RecommendedFoodApp
     config.load_defaults 6.1
 
     config.i18n.default_locale = :ja
-    config.time_zone = 'Asia/Tokyo'
+    config.time_zone = "Asia/Tokyo"
 
     # Configuration for the application, engines, and railties goes here.
     #
