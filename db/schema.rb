dc33901765b032d2f35ab6977673ec07650bf879
teru <titlt.aiw6@gmail.com>
2021-11-09 08:38:24 +0900

    カラムの追加、投稿フォーム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_06_010623) do
+ActiveRecord::Schema.define(version: 2021_11_08_232901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_11_06_010623) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
