e50be7c970dc866ead32670d0652bb1753c52a08
teru <titlt.aiw6@gmail.com>
2021-11-09 17:19:39 +0900

    一覧表示にいいね機能実装

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,6 +5,6 @@ class Food < ApplicationRecord
   validates :name, presence: true
   mount_uploader :image, ImageUploader
   def liked_by?(user)
-    likes.exists?(user_id: user.id)
+    likes.any? { |like| like.user_id == user.id }
   end
 end
